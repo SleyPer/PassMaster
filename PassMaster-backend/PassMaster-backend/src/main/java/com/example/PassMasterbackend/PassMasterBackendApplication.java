68f8d0f5048f561073a2c3c90d4f5a1e26e04e6b
ThomasPREVOST <t.prevost@attineos.com>
2023-09-20 15:21:13 +0200

    Initialisation of the project

---
+++ PassMaster-backend/PassMaster-backend/src/main/java/com/example/PassMasterbackend/PassMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PassMasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassMasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassMasterBackendApplication.class, args);
+	}
+
+}
---
+++ PassMaster-backend/PassMaster-backend/src/test/java/com/example/PassMasterbackend/PassMasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PassMasterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassMasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
